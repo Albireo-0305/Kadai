d557bfd8140fdb31a4b6b4f29a2a1cfaac2e592a
Albireo <l2404825418as@gmail.com>
2025-05-07 11:39:54 +0900

    S10_Kadai_end

--- src/main/java/com/example/S7/data/StudentsCourses.java
+++ src/main/java/com/example/S7/data/StudentsCourses.java
@@ -1,17 +1,16 @@
 package com.example.S7.data;
 
-import javax.xml.crypto.Data;
+import java.util.Date;
 import lombok.Getter;
 import lombok.Setter;
 
-@Getter
 @Setter
+@Getter
 public class StudentsCourses {
 
   public int courseid;
   public int studentid;
   public String coursename;
-  public Data startdate;
-  public Data expectedenddate;
-
+  public Date startdate;
+  public Date expectedenddate;
 }
--- src/main/java/com/example/S7/repository/StudentRepository.java
+++ src/main/java/com/example/S7/repository/StudentRepository.java
@@ -14,6 +14,4 @@ public interface StudentRepository {
 
   @Select("SELECT * FROM Students_Courses")
   List<StudentsCourses> AllStudentsCourses();
-
-
 }
