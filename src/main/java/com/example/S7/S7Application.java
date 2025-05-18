fa864c875a23fb717bf05c8187ec3a5a80237604
Albireo <l2404825418as@gmail.com>
2025-05-18 19:06:23 +0900

    Kadai_14 完了

--- src/main/java/com/example/S7/S7Application.java
+++ src/main/java/com/example/S7/S7Application.java
@@ -1,14 +1,8 @@
 package com.example.S7;
 
-import com.example.S7.data.Student;
-import com.example.S7.data.StudentsCourses;
-import com.example.S7.domain.StudentDetail;
-import java.util.List;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+
 
 @SpringBootApplication
 public class S7Application {
--- src/main/java/com/example/S7/controller/StudentController.java
+++ src/main/java/com/example/S7/controller/StudentController.java
@@ -9,10 +9,12 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class StudentController {
 
   public StudentService service;
@@ -24,12 +26,14 @@ public class StudentController {
     this.converter = converter;
   }
 
+
   @GetMapping("/studentList")
-  public List<StudentDetail> getStudentList() {
+  public String getStudentList(Model model) {
     List<Student> students = service.searchStudentList();
     List<StudentsCourses> studentsCourses = service.serchStudentsCoursesList();
 
-    return converter.convertStudentDetails(students, studentsCourses);
+    model.addAttribute("studentList",converter.convertStudentDetails(students, studentsCourses));//こっちstudentListはテンプレートの名前
+    return "studentList";//ファイル名
   }
 
 
